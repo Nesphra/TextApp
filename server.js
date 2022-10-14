c5d7c80942804267f5eeb7e0d9c7403350396b14
Terence Dumas <dumast@awty.org>
2022-10-14 15:49:49 -0500

    index file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
 // const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
-app.use(app.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
   res.sendFile('/index.html');
